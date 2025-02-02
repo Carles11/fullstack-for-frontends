5a4a8a6306c4f7d128de64b6b5a884702482f68d
carles <c.delriofrances@gmail.com>
2025-02-02 18:11:44 +0000

    code update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer(function (req, res){
 
-	res.write("On the way to being a full-stack engineer!")
+	res.write("On the way to being a full-stack engineer, boy!")
 	res.end();
 
 }).listen(3000);
